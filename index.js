a1ff720ad570cace83efa590cbe3ccf1d15fb52f
Mohi Uddin Mahim <mohi35-2790@diu.edu.bd>
2021-12-02 21:49:02 +0600

    basic structure

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ltkb6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('swe-project-portal');
+        const movies = database.collection('projects');
+        // Query for a movie that has the title 'Back to the Future'
+        const query = { title: 'Back to the Future' };
+        const movie = await movies.findOne(query);
+        console.log(movie);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello doctors portal!');
+})
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
+})
